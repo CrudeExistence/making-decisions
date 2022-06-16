ec8cd9808ed0f32223f9af94a26c3927da30e40d
Travis Durrant <kinglink89@yahoo.com>
2022-06-16 12:53:00 -0600

    Another session of work

--- arrays.js
+++ arrays.js
@@ -2,3 +2,32 @@
     In this file, we'll be continuing our story
     with Jon being allowed to flee his fight with Jamie.
 */
+
+let backpack = [];
+
+backpack.push("Lightsaber")
+backpack.push("Robe", "Food")
+
+// console.log(backpack)
+let belt = backpack.shift()
+
+backpack.splice(1, 0, "coat")
+
+let bag = []
+
+bag.unshift("Flint", "Blanket", "Toothbrush")
+
+backpack.push(bag.slice(0, bag.length))
+
+console.log(`The backpack has: ${backpack}`)
+console.log(`The belt has: ${belt}`)
+console.log(`The bag has: ${bag}`)
+console.log(backpack)
+
+for (let i=0; i < bag.length; i++) {
+    console.log(bag[i])
+}
+
+for (let i=0; i < 3; i++) {
+    console.log(backpack[i])
+}
